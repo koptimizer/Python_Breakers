a541ca97e3acb24a814af82cd88db6c00d57ddd4
LightBell <rhkswhdwkd@naver.com>
2019-09-21 23:43:05 +0900

    자료구조
    190921 : 넘 졸려서 조금만...

---
+++ pr_dc.py
@@ -0,0 +1,10 @@
+
+#list 자료구조
+
+praclist = ['apple', 'banana', 'carrot', 'danger', 'elephant' ]
+praclist.append('fukuoka')
+praclist.insert(6, 'guild')
+praclist.remove('apple')
+print(praclist.pop()) # 선입후출
+
+print(praclist)
