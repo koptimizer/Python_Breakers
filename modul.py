d04e8f54968edb8ab004edfdcfba9f3130eda26c
LightBell <rhkswhdwkd@naver.com>
2019-10-06 23:34:56 +0900

    191006
    모듈

--- modul.py
+++ modul.py
@@ -1,5 +1,18 @@
-import mod.py
+from Scripts import mod as f
+import sys
 
-mod.fib(1000)
-mod.fib2(100)
-mod.__name__
+f.fib(1000)
+print(f.fib2(100))
+print(f.__name__)
+
+from Scripts.mod import fib as fibo
+
+fibo(500)
+
+#모듈 정의 이름 찾기
+print(dir(f))
+print(dir(sys))
+# 인자 없으면 현재 정의한 이름들 모두 나열
+print(dir())
+
+
